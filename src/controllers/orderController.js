86baf81b6feae040b5c4287e4baa0c2beced4dd3
Richard-vinu <richardnova1999@gmail.com>
2022-10-28 13:14:10 +0530

    cart

---
+++ src/controllers/orderController.js
@@ -0,0 +1,35 @@
+import User from '../models/User.js'
+import Product from '../models/Product.js'
+import Cart from '../models/Cart.js'
+import Order from '../models/Order.js'
+
+let createOrders =async (req,res)=>{
+    try{
+        let data = req.body
+        let userId = req.params.userId
+        let user = await User.findById(userId)
+        if(!user) return res.status(404).send({message:"user Not found"})
+
+        let cart = await Cart.findById(req.body.cartId)
+
+        if(!cart) return res.status(404).send({message:"Product Not found"})
+        
+        data.userId = userId
+        data.items = cart.items
+        data.totalPrice = cart.totalPrice
+        data.totalItems = cart.totalItems
+        data.totalQuantity = 0
+        cart.items.map(x=>{data.totalQuantity+=x.quantity})
+
+        let order = await Order.create(data)
+    
+        res.status(201).send({status:true,data:order})
+
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+
+export {createOrders}
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,23 +2,25 @@ import express from 'express'
 
 let route = express.Router()
 import {userRegister,userLogin} from '../controllers/userController.js'
-
 import {createProduct, getProducts} from '../controllers/productController.js'
-
 import { createCart } from '../controllers/cartController.js'
+import { createOrders } from '../controllers/orderController.js'
 
 route.get('/test',(req,res)=>res.send({sucess:1,message:`Api fired`}))
 
+//user APis
 route.post('/user/signup',userRegister)
-
 route.post('/user/login',userLogin)
 
+//product Apis
 route.post('/products',createProduct)
-
 route.get('/products',getProducts)
 
-
+//cart Apis
 route.post('/users/:userId/cart',createCart)
+
+//order Apis
+route.post('/users/:userId/orders',createOrders)
 
 
 export default route
